c2eb28cd50104bd922127f4e26bab5720e17197d
Vo Saan <vo.saan@gmail.com>
2021-01-08 15:03:13 +0200

    'username' instead of 'name'

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,7 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name')->unique();
+            $table->string('username')->unique();
             $table->string('password');
             $table->boolean('is_admin')->default(0);
             $table->timestamps();
