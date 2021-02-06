a3169eddd360d50b3cf6730d05d7ccb670aaa33d
Vo Saan <vo.saan@gmail.com>
2021-02-06 13:51:53 +0200

    footer

---
+++ resources/views/frontend/parts/footer.blade.php
@@ -0,0 +1,18 @@
+<footer class="footer py-4">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-lg-4 text-lg-left">Copyright © Hygge`dent {{ date('Y') }}</div>
+            <div class="col-lg-4 my-3 my-lg-0">
+                <a class="btn btn-dark btn-social mx-2" href="https://www.facebook.com/hyggedent/" target="_blank">
+                    <i class="fab fa-facebook-f"></i>
+                </a>
+                <a class="btn btn-dark btn-social mx-2" href="https://www.instagram.com/hygge.dent.zp/" target="_blank">
+                    <i class="fab fa-instagram"></i>
+                </a>
+            </div>
+            <div class="col-lg-4 text-lg-right">
+
+            </div>
+        </div>
+    </div>
+</footer>
